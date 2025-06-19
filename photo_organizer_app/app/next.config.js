fc3ed1ce5738055d6ff6c08cebe6886d1a166ac3
beltran-h <beltran.horisberger@gmail.com>
2025-06-19 16:20:18 +0200

    next.config.js

--- photo_organizer_app/app/next.config.js
+++ photo_organizer_app/app/next.config.js
@@ -1,19 +1,22 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: false,
   },
-  images: { unoptimized: true },
+  images: { 
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  // Configuración para funcionar bien en Vercel
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
